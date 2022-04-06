7cf6a658b92cc021eea5953d7fcc1491dedf7afe
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-04-06 09:30:30 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (59.70%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -15,12 +15,12 @@ public class Solution {
         ListNode slow = head; ListNode fast = head; 
         
         while(fast !=null && fast.next != null){
+           
             slow = slow.next;
-            fast=fast.next.next;
-            
-            if(slow == fast){
+            fast=fast.next.next;    
+             if(slow == fast){
                 return true;
-            }           
+            } 
         }
         return false;   
     }
