014f3f51e46008f89195a94833d14f1f6c502bc8
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-07-07 21:33:51 +0530

    Time: 83 ms (33.57%), Space: 14.5 MB (72.41%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,19 +9,14 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *ptr1 = headA, *ptr2 = headB; //pointers on each head.
-        while(ptr1 != ptr2){
-            if(ptr1)
-                ptr1 = ptr1->next;
-            else
-                ptr1 = headB;
-            
-            if(ptr2)
-                ptr2 = ptr2->next;
-            else
-                ptr2 = headA;
+        //see this is the way just see start at head1 and head2 and start traversing  and if any one is null then assign that to opposite head so that the extra length larger ll be will covered
+        //just see with some example u will understand dont panic just see the code and understand
+        ListNode* temp1=headA;
+        ListNode* temp2=headB;
+        while(temp1!=temp2){
+            temp1=(temp1==NULL)?headB:temp1->next;
+             temp2=(temp2==NULL)?headA:temp2->next;
         }
-        return ptr1;
-    
+        return temp1;
     }
 };
