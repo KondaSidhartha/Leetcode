dc299172b3fe82e0e84e843f3b720eeb4e76f28a
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-08-10 11:30:05 +0530

    Time: 183 ms (64.33%), Space: 79.9 MB (32.29%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    vector<vector<string>> ans;
+    bool palin(string S)
+{
+    for (int i = 0; i < S.length() / 2; i++) {
+        if (S[i] != S[S.length() - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+    void find(string s,vector<string> &temp,int idx){
+        
+        if(idx==s.size()){ans.push_back(temp); return ;}
+        for(int i=idx;i<s.size();i++){
+                if(palin(s.substr(idx,i-idx+1))){
+                    temp.push_back(s.substr(idx,i-idx+1));
+                    find(s,temp,i+1);
+                    temp.pop_back();
+                }
+        }
+    }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> p;
-        fun(s,ans,p,0);
+        vector<string> temp;
+        find(s,temp,0);
         return ans;
-    }
-    void fun(string s,vector<vector<string>> &ans,vector<string> &p,int index){
-        if(index==s.size()){
-            ans.push_back(p);
-            return;
-        }
-        for(int i=index;i<s.size();++i){
-            if(palindrome(s,index,i)){
-                p.push_back(s.substr(index,i-index+1));
-                fun(s,ans,p,i+1);
-                p.pop_back();
-            }
-        }
-    }
-    bool palindrome(string s,int start,int last){
-        while(start<=last){
-            if(s[start++]!=s[last--])return false;
-        }
-        return true;
     }
 };
