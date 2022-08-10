1efff8b91c111c76c3e49458d2552776f5b75701
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-08-10 10:51:36 +0530

    Time: 34 ms (34.27%), Space: 15.6 MB (42.43%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -5,14 +5,14 @@ public:
         if(curr==target)ans.push_back(temp);
         
         for(int i=idx;i<candidates.size();i++){
-            if(curr+candidates[i]>target)return ;
+            if(curr+candidates[i]>target)continue;
             temp.push_back(candidates[i]);
             find(candidates,target,curr+candidates[i],temp,i);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
+        //sort(candidates.begin(),candidates.end());
         vector<int> temp;
         find(candidates,target,0,temp,0);
         return ans;
