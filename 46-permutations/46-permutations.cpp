94e08d4abc1784f90f45bd7f0aa329f43d718e5b
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-08-10 20:34:37 +0530

    Time: 3 ms (82.69%), Space: 8 MB (37.26%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,21 +3,19 @@ public:
     vector<vector<int>> ans;
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> temp;
-        search(nums,temp);
+        search(nums,temp,0);
         return ans;
     }
-    void search(vector<int> &nums, vector<int> &temp){
-        if(temp.size()==nums.size()){
+    void search(vector<int> &nums, vector<int> &temp,int idx){
+        if(idx==nums.size()){
             ans.push_back(temp);
         }
-        
-        for(int i=0;i<nums.size();i++){
-            auto it=find(temp.begin(),temp.end(),nums[i]);
-            if(it!=temp.end())continue;
-            
-            temp.push_back(nums[i]);
-            search(nums,temp);
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[i],nums[idx]);
+            temp.push_back(nums[idx]);
+            search(nums,temp,idx+1);
             temp.pop_back();
+            swap(nums[i],nums[idx]);
         }
     }
 };
