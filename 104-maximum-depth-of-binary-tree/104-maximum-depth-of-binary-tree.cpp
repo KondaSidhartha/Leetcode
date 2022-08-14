6f496e2bc8a4c27f4c34b57cdbbe0d4328966646
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-08-14 14:11:24 +0530

    Time: 8 ms (86.34%), Space: 18.9 MB (14.24%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -11,13 +11,12 @@
  */
 class Solution {
 public:
-    int find(TreeNode* root,int a){
-        if(root==NULL)return a-1;
-        
-        a=max(find(root->left,a+1),find(root->right,a+1));
-        return a;
-    }
     int maxDepth(TreeNode* root) {
-        return find(root,1);
+        if(root==NULL)return 0;
+        
+        int left=maxDepth(root->left);
+        int right=maxDepth(root->right);
+        
+        return max(left,right)+1;
     }
 };
