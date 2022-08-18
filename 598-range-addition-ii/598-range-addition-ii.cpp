a11677590f447a7d1703331349e48577f91451f8
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-08-18 23:17:44 +0530

    Time: 15 ms (64.11%), Space: 11 MB (85.48%) - LeetHub

--- 598-range-addition-ii/598-range-addition-ii.cpp
+++ 598-range-addition-ii/598-range-addition-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maxCount(int m, int n, vector<vector<int>>& ops) {
-        if(ops.size()==0)return m*n;
-        int a=INT_MAX;
-        int b=INT_MAX;
+       // if(ops.size()==0)return m*n;
+        int a=m;
+        int b=n;
         for(int i=0;i<ops.size();i++){
               a=min(a,ops[i][0]);
             b=min(b,ops[i][1]);
