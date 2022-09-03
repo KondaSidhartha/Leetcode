a922bdaa0819812f77b67c2601682f3c427a64f9
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2022-09-03 14:47:27 +0530

    Time: 6 ms (59.64%), Space: 13.9 MB (36.99%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> ans;
+    void solve(int n,string s,int a,int b){
+        if(s.size()==2*n)ans.push_back(s);
+        if(a<n){
+            solve(n,s+'(',a+1,b);
+        }
+        if(b<a){
+            solve(n,s+')',a,b+1);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        solve(n,"(",1,0);
+        return ans;
+    }
+};
