10bd46c4e70aa6c6ac3f8d64bab35a9c868ef89d
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2023-03-25 13:56:39 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -6,45 +6,35 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
+
 class Solution{
 public:
-    vector<int> dr={1,-1,0,0};
-    vector<int> dc={0,0,1,-1};
-    
-    bool isValid(int i,int j,int n,int m){
-        return ((i>=0 && i<n) && (j>=0 && j<m));
+    void dfs(int i, int j, vector<vector<char>> &mat){
+        if(i<0 || i>=mat.size() || j<0 || j>=mat[0].size() || mat[i][j]=='X' || mat[i][j]=='L')return; 
+        mat[i][j]='L';
+        dfs(i+1,j,mat);
+        dfs(i-1,j,mat);
+        dfs(i,j+1,mat);
+        dfs(i,j-1,mat);
+        
     }
-    
-    void DFS(int i,int j,vector<vector<char>>& grid,int n,int m){
-        grid[i][j]='F';
-        for(int k=0;k<4;k++){
-            int nr=i+dr[k],nc=j+dc[k];
-            if(isValid(nr,nc,n,m) && grid[nr][nc]=='O')
-                DFS(nr,nc,grid,n,m);
-        }
-    }
-    
-    vector<vector<char>> fill(int n, int m, vector<vector<char>> grid)
-    {
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {   
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(i==0 || j==0 || i==n-1 || j==m-1){
-                    if(grid[i][j]=='O')
-                        DFS(i,j,grid,n,m);
+                    if(mat[i][j]=='O')dfs(i,j,mat);
                 }
             }
         }
-        for(auto &it:grid){
-            for(auto &gt:it){
-                if(gt=='F')
-                    gt='O';
-                else
-                    gt='X';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]=='L')mat[i][j]='O';
+                else mat[i][j]='X';
             }
         }
-        return grid;
+        return mat;
     }
-
 };
 
 //{ Driver Code Starts.
