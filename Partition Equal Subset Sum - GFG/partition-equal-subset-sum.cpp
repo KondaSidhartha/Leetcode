ad8a58af8e2d806404889e0410f36c30560a1211
Konda Sidhartha <77008318+KondaSidhartha@users.noreply.github.com>
2023-07-13 10:04:47 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,39 +9,29 @@ using namespace std;
 
 class Solution{
 public:
-bool subsetsum(vector<int>& nums,int sum){
-        int n=nums.size();
-        int m=sum;
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        for(int i=0;i<m;i++){
-            dp[0][i]=false;
+    vector<vector<int>> dp;
+    int rec(int ind, int arr[],int tar,int n){
+        if(tar==0){
+            return 1;
         }
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
+        if(ind>=n)return 0;
+        if(dp[ind][tar]!=-1)return dp[ind][tar];
+        int ans=0;
+        ans|=rec(ind+1,arr,tar,n);
+        if(tar>=arr[ind]){
+            ans|=rec(ind+1,arr,tar-arr[ind],n);
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][m];
+        return dp[ind][tar]=ans;
     }
     int equalPartition(int N, int arr[])
     {
         int sum=0;
-        vector<int> nums;
-        for(int i=0;i<N;i++)nums.push_back(arr[i]);
-        for(auto i:nums)sum+=i;
-        if(sum%2==1)return false;
-        else{
-            return subsetsum(nums,sum/2);
+        for(int i=0;i<N;i++){
+            sum+=arr[i];
         }
-        return false;
+        if(sum%2!=0)return 0;
+        dp.resize(N+1,vector<int>((sum/2)+1,-1));
+        return rec(0,arr,sum/2,N);
     }
 };
 
